2f2b681a44b16bf69ee7d5bb9790a1e329a8c0a7
杨小刚 <nulijiuhao712@126.com>
2019-03-20 18:17:17 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { Button } from 'ant-design-vue';
+Vue.use(Button)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
